5e89d622e0086ebbef548b0871cf7d239aa1a855
Ryan Trattles <ryan.trattles@gmail.com>
2016-01-11 15:30:43 -0500

    - currently getting error with simple router. Unsure as to why.

--- client/entry.jsx
+++ client/entry.jsx
@@ -2,27 +2,27 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
-//import Routes from './../common/routes/Routes.js';
-
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 import reducers from './../common/Reducers/index.js';
 import thunkMiddleware from 'redux-thunk';
+import { syncHistory, routeReducer } from 'redux-simple-router';
 
+const reducer = combineReducers(Object.assign({}, reducers, {routing: routeReducer }));
 const initialState = window.__INITIAL_STATE__;
-const createStoreWithMiddleware = applyMiddleware( thunkMiddleware)(createStore);
-const store = createStoreWithMiddleware(reducers(initialState));
+const simpleRouter = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, simpleRouter)(createStore);
+
+const store = createStoreWithMiddleware(reducer(initialState));
+simpleRouter.syncHistoryToStore(store);
 
 //Components
-import App from './../common/components/App.jsx';
-import Name from './../common/components/Name.jsx';
-import Profile from './../common/components/Profile.jsx';
-import Messages from './../common/components/Messages.jsx';
 
 import routeConfig from './../common/routes/Routes.js';
-
 import Radium, {Style, StyleRoot} from 'radium';
 import Normalize from './../common/styles/Normalize.js';
+
+
 
 ReactDOM.render(
 	<Provider store={store}>
